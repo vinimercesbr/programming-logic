e81f40442d160a7b9176107125ed9b08343de94e
Vinícius Bruno <vinimerces2016@gmail.com>
2025-04-02 00:46:00 +0000

    teste java

---
+++ ex11.java
@@ -0,0 +1,13 @@
+
+public class Ex3 {
+
+    public static void main(String[] args) {
+
+        int sucessor = 16;
+        int antecessor = sucessor - 1;
+
+        System.out.println("o antecessor é" + antecessor);
+
+    }
+
+}
